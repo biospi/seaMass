ae84f86b054ae14f78b67dfd32c49e441ead67ac
Andrew Dowsey <andrew.dowsey@liverpool.ac.uk>
2016-10-12 13:25:44 +0100

    added a max_peptides option because a protein with 1000 peptides so stopping things finishing sensibly

--- submit/import.R
+++ submit/import.R
@@ -5,6 +5,7 @@ if (length(commandArgs(T)) > 0 & commandArgs(T)[1]=="HPC")
   
   load("data.Rdata")
   load("design.Rdata")
+  load("parameters.Rdata")
   
   library(plyr)
   library(reshape2)
@@ -16,6 +17,17 @@ if (length(commandArgs(T)) > 0 & commandArgs(T)[1]=="HPC")
   data.index <- ddply(data, .(ProteinID), function(data.one) {  
     pid <- data.one$ProteinID[1]
     print(paste0(Sys.time()," [import() Processing protein ",pid,"]"))    
+    
+    # filter by max_peptides
+    if("max_peptides" %in% parameters$Key)
+    {
+        # order peptides by maximum ident confidence then maximum precursor signal
+        data.one <- data.one[order(-data.one$Conf,-data.one[,precursorSignal]),] 
+        data.one$Peptide <- factor(data.one$Peptide, levels=unique(data.one$Peptide))
+        # keep the max_peptides number of peptides with greatest number of readings
+        tb <- table(data.one$Peptide)  
+        data.one <- data.one[data.one$Peptide %in% names(tb[order(tb, decreasing=T)])[1:as.integer(parameters$Value[parameters$Key=="max_peptides"])],]
+    }
     
     # extract data for mixed model
     data.tmp <- melt(data.one, variable.name='Channel', value.name='Area',
