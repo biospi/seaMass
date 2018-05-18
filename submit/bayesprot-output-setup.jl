4e6b846dcf48c28fc88caae1d34c2afab55c6a9f
Ranjeet Bhamber <ranjeet@bhamber.com>
2018-05-18 17:03:34 +0100

    samplestats added

--- submit/bayesprot-output-setup.jl
+++ submit/bayesprot-output-setup.jl
@@ -2,6 +2,7 @@ try symlink("../../import/results/index.Rdata", "output/results/index.Rdata") en
 try symlink("../../input/parameters.Rdata", "output/results/parameters.Rdata") end
 try symlink("../../input/design.Rdata", "output/results/design.Rdata") end
 try symlink("../../plots/results/stats/", "output/results/stats") end
+try symlink("../../plots/results/samplestats/", "output/results/samplestats") end
 
 try mkdir("output/error") end
 try mkdir("output/out") end
