44db4bb5207e11f93994e9d79bf60402ec5aeed4
Andrew Dowsey <andrew.dowsey@bristol.ac.uk>
2018-06-19 12:47:12 +0100

    added ggfortify package

--- submit/installRpackages.R
+++ submit/installRpackages.R
@@ -1,5 +1,6 @@
 repos <- "http://mirrors.ebi.ac.uk/CRAN/"
 install.packages("data.table", dependencies=T, repos=repos)
 install.packages("ggplot2", dependencies=T, repos=repos)
+install.packages("ggfortify", dependencies=T, repos=repos)
 install.packages("MCMCglmm", dependencies=T, repos=repos)
 install.packages("mcgibbsit", dependencies=T, repos=repos)
