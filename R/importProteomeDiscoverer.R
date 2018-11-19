de18354a33fdef22169f4b6255ae2fcedf5c8499
Andrew Dowsey <andrew.dowsey@bristol.ac.uk>
2018-11-19 20:11:57 +0000

    importProteomeDiscovere now supports SILAC data

--- R/importProteomeDiscoverer.R
+++ R/importProteomeDiscoverer.R
@@ -8,17 +8,17 @@
 importProteomeDiscoverer <- function(datafile, dd.fractions) {
   # read ProteomeDiscoverer PSMs
   message(paste0("reading: ", datafile, "..."))
-  dd.raw <- fread(datafile, check.names = T)
+  dd.raw <- fread(datafile)
 
   # only use rows that ProteomeDiscoverer uses for quant (TODO: reconsider)
-  dd.raw <- dd.raw[Peptide.Quan.Usage == "Use",]
-  dd.raw <- dd.raw[Quan.Info == "Unique",]
+  dd.raw <- dd.raw[`Peptide Quan Usage` == "Use",]
+  dd.raw <- dd.raw[`Quan Info` == "Unique",]
 
-  # fractions are identified by "Spectrum.File"
-  dd.raw <- merge(dd.raw, dd.fractions[, list(Spectrum.File = Fraction, Run)])
+  # fractions are identified by "Spectrum File"
+  dd.raw <- merge(dd.raw, dd.fractions[, .(`Spectrum File` = Fraction, Run)])
 
   # only retain the most confidenct and most intense spectrum for each feature (TODO: make option)
-  dd.raw[, Feature := factor(paste0(dd.raw$Sequence, " : ", dd.raw$Modifications, " : ", dd.raw$Charge, "+ : ", dd.raw$Spectrum.File))]
+  dd.raw[, Feature := factor(paste0(dd.raw$Sequence, " : ", dd.raw$Modifications, " : ", dd.raw$Charge, "+ : ", dd.raw$`Spectrum File`))]
   setorder(dd.raw, Feature, Confidence, -Intensity)
   dd.raw <- unique(dd.raw, by = "Feature")
 
@@ -26,28 +26,31 @@ importProteomeDiscoverer <- function(datafile, dd.fractions) {
 
   # create wide data table
   dd.wide <- dd.raw[ , list(
-    Protein = factor(Master.Protein.Accessions),
+    Protein = factor(`Master Protein Accessions`),
     Peptide = factor(paste(Sequence, ":", Modifications)),
-    Feature = factor(paste(Spectrum.File, ":", First.Scan)),
+    Feature = factor(paste(`Spectrum File`, ":", `First Scan`)),
     Assay = Run
   )]
-  if("X126C" %in% colnames(dd.raw)) dd.wide$Label.126C <- dd.raw$X126C
-  if("X126N" %in% colnames(dd.raw)) dd.wide$Label.126N <- dd.raw$X126N
-  if("X126" %in% colnames(dd.raw)) dd.wide$Label.126 <- dd.raw$X126
-  if("X127C" %in% colnames(dd.raw)) dd.wide$Label.127C <- dd.raw$X127C
-  if("X127N" %in% colnames(dd.raw)) dd.wide$Label.127N <- dd.raw$X127N
-  if("X127" %in% colnames(dd.raw)) dd.wide$Label.127 <- dd.raw$X127
-  if("X128C" %in% colnames(dd.raw)) dd.wide$Label.128C <- dd.raw$X128C
-  if("X128N" %in% colnames(dd.raw)) dd.wide$Label.128N <- dd.raw$X128N
-  if("X128" %in% colnames(dd.raw)) dd.wide$Label.128 <- dd.raw$X128
-  if("X129C" %in% colnames(dd.raw)) dd.wide$Label.129C <- dd.raw$X129C
-  if("X129N" %in% colnames(dd.raw)) dd.wide$Label.129N <- dd.raw$X129N
-  if("X129" %in% colnames(dd.raw)) dd.wide$Label.129 <- dd.raw$X129
-  if("X130C" %in% colnames(dd.raw)) dd.wide$Label.130C <- dd.raw$X130C
-  if("X130N" %in% colnames(dd.raw)) dd.wide$Label.130N <- dd.raw$X130N
-  if("X131C" %in% colnames(dd.raw)) dd.wide$Label.131C <- dd.raw$X131C
-  if("X131N" %in% colnames(dd.raw)) dd.wide$Label.131N <- dd.raw$X131N
-  if("X131" %in% colnames(dd.raw)) dd.wide$Label.131 <- dd.raw$X131
+  if("Light" %in% colnames(dd.raw)) dd.wide$Label.Light <- dd.raw$Light
+  if("Medium" %in% colnames(dd.raw)) dd.wide$Label.Medium <- dd.raw$Medium
+  if("Heavy" %in% colnames(dd.raw)) dd.wide$Label.Heavy <- dd.raw$Heavy
+  if("126C" %in% colnames(dd.raw)) dd.wide$Label.126C <- dd.raw$`126C`
+  if("126N" %in% colnames(dd.raw)) dd.wide$Label.126N <- dd.raw$`126N`
+  if("126" %in% colnames(dd.raw)) dd.wide$Label.126 <- dd.raw$`126`
+  if("127C" %in% colnames(dd.raw)) dd.wide$Label.127C <- dd.raw$`127C`
+  if("127N" %in% colnames(dd.raw)) dd.wide$Label.127N <- dd.raw$`127N`
+  if("127" %in% colnames(dd.raw)) dd.wide$Label.127 <- dd.raw$`127`
+  if("128C" %in% colnames(dd.raw)) dd.wide$Label.128C <- dd.raw$`128C`
+  if("128N" %in% colnames(dd.raw)) dd.wide$Label.128N <- dd.raw$`128N`
+  if("128" %in% colnames(dd.raw)) dd.wide$Label.128 <- dd.raw$`128`
+  if("129C" %in% colnames(dd.raw)) dd.wide$Label.129C <- dd.raw$`129C`
+  if("129N" %in% colnames(dd.raw)) dd.wide$Label.129N <- dd.raw$`129N`
+  if("129" %in% colnames(dd.raw)) dd.wide$Label.129 <- dd.raw$`129`
+  if("130C" %in% colnames(dd.raw)) dd.wide$Label.130C <- dd.raw$`130C`
+  if("130N" %in% colnames(dd.raw)) dd.wide$Label.130N <- dd.raw$`130N`
+  if("131C" %in% colnames(dd.raw)) dd.wide$Label.131C <- dd.raw$`131C`
+  if("131N" %in% colnames(dd.raw)) dd.wide$Label.131N <- dd.raw$`131N`
+  if("131" %in% colnames(dd.raw)) dd.wide$Label.131 <- dd.raw$`131`
 
   # melt label counts
   dd <- melt(dd.wide, variable.name = "Label", value.name = "Count",
--- inst/hpc/slurm.sh
+++ inst/hpc/slurm.sh
@@ -15,11 +15,11 @@ EXITCODE=$?
 
 # clean up
 if [[ $EXITCODE != 0 ]]; then
-  scancel $MODEL1 $STUDY $MODEL2 $QUANT $QPROT $DE
+  scancel $MODEL $STUDY $MODEL2 $QUANT $QPROT $DE
   echo Failed to submit jobs!
 else
   echo Submitted jobs! To cancel execute:
-  echo scancel $MODEL1 $STUDY $MODEL2 $QUANT $QPROT $DE
+  echo scancel $MODEL $STUDY $MODEL2 $QUANT $QPROT $DE
 fi
 
 popd > /dev/null
