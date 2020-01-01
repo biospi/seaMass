934a614f788fb30ef9c835503bbeca2fea677e9b
Andrew Dowsey <andrew.dowsey@manchester.ac.uk>
2020-01-01 21:59:07 +0000

    incrementing to v1.0.1.0

--- R/seaMass.R
+++ R/seaMass.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0("seaMass v", packageVersion("seaMass"), "  |  © 2019  BIOSP", utf8::utf8_encode("\U0001f441"), "  Laboratory"))
+  packageStartupMessage(paste0("seaMass v", packageVersion("seaMass"), "  |  © 2019-2020  BIOSP", utf8::utf8_encode("\U0001f441"), "  Laboratory"))
   packageStartupMessage("This program comes with ABSOLUTELY NO WARRANTY.")
   packageStartupMessage("This is free software, and you are welcome to redistribute it under certain conditions.")
 }
